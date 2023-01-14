2505fe2bd945177c2462c58fd3c849c066b7239a
bloobsky <bloobsky@gmail.com>
2023-01-14 14:00:58 +0000

    Added eurosign for price.

---
+++ Models/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShopApp.Models
+{
+    public class Food
+    {
+        public string Picture { get; set; }
+        // public string Quantity { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
